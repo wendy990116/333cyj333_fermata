57e91479da0bb0a2f46fcb58bbc1eae081a6aefd
wendy990116 <wendy990116@gmail.com>
2025-11-26 16:50:16 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAFYAQO__cSHFpZUZbEImfQusyTZoSQ15Y",
+  authDomain: "cyj333-g7.firebaseapp.com",
+  projectId: "cyj333-g7",
+  storageBucket: "cyj333-g7.firebasestorage.app",
+  messagingSenderId: "990374083387",
+  appId: "1:990374083387:web:e78405fbcc133aed28151e"
 });
 
 const messaging = firebase.messaging();
